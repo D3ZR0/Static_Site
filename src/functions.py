bc15d8c05fb9ca4e1b84513e06feeae5aa360728
D3ZR0 <dwilcox0526@hotmail.com>
2025-10-09 00:38:48 -0400

    finished functions and test

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,6 @@
 from htmlnode import *
 from textnode import *
+from textnode import TextType
 
 def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.TEXT:
@@ -17,4 +18,24 @@ def text_node_to_html_node(text_node):
     else:
         raise ValueError(f"Unknown TextType: {text_node.text_type}")
     
+    
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        parts = node.text.split(delimiter)
+        if len(parts) %2 == 0:
+            raise Exception("invalid markdown syntax, unbalanced delimiter")
+        for i, part in enumerate(parts):
+            node_type = TextType.TEXT if i %2 == 0 else text_type    
+            new_nodes.append(TextNode(part, node_type))
+    return new_nodes
+
+            
+        
+
+
+        
     
---
+++ src/test_functions.py
@@ -0,0 +1,87 @@
+import unittest
+from functions import text_node_to_html_node, split_nodes_delimiter
+from htmlnode import HTMLNode, LeafNode, ParentNode
+from textnode import TextNode, TextType
+
+class TestConverters(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_bold(self):
+        node = TextNode("This is bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold text")
+    
+    def test_italic(self):
+        node = TextNode("This is italic text", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is italic text")
+
+    def test_code(self):
+        node = TextNode("print('Hello, world!')", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "CODE")
+        self.assertEqual(html_node.value, "print('Hello, world!')")
+    
+    def test_link(self):
+        node = TextNode("OpenAI", TextType.LINK, "https://www.openai.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "OpenAI")
+        self.assertEqual(html_node.props, {"href": "https://www.openai.com"})
+    
+    def test_split_nodes_delimiter_bold(self):
+        nodes = [TextNode("This is **bold** text", TextType.TEXT)]
+        new_nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0], TextNode("This is ", TextType.TEXT))
+        self.assertEqual(new_nodes[1], TextNode("bold", TextType.BOLD))
+        self.assertEqual(new_nodes[2], TextNode(" text", TextType.TEXT))
+
+    def test_split_nodes_delimiter_italic(self):
+        nodes = [TextNode("This is *italic* text", TextType.TEXT)]
+        new_nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0], TextNode("This is ", TextType.TEXT))
+        self.assertEqual(new_nodes[1], TextNode("italic", TextType.ITALIC))
+        self.assertEqual(new_nodes[2], TextNode(" text", TextType.TEXT))
+    
+    def test_split_nodes_delimiter_unbalanced(self):
+        nodes = [TextNode("This is **bold text", TextType.TEXT)]
+        with self.assertRaises(Exception) as context:
+            split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        self.assertTrue("invalid markdown syntax, unbalanced delimiter" in str(context.exception))
+    
+    def test_split_nodes_delimiter_multiple(self):
+        nodes = [TextNode("This is **bold** and *italic* text", TextType.TEXT)]
+        new_nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        new_nodes = split_nodes_delimiter(new_nodes, "*", TextType.ITALIC)
+        self.assertEqual(len(new_nodes), 5)
+        self.assertEqual(new_nodes[0], TextNode("This is ", TextType.TEXT))
+        self.assertEqual(new_nodes[1], TextNode("bold", TextType.BOLD))
+        self.assertEqual(new_nodes[2], TextNode(" and ", TextType.TEXT))
+        self.assertEqual(new_nodes[3], TextNode("italic", TextType.ITALIC))
+        self.assertEqual(new_nodes[4], TextNode(" text", TextType.TEXT))
+    
+    def test_split_node_delimiter_multiple_nodes(self):
+        nodes = [
+            TextNode("This is **bold** text", TextType.TEXT),
+            TextNode("Another *italic* text", TextType.TEXT)
+        ]
+        new_nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        new_nodes = split_nodes_delimiter(new_nodes, "*", TextType.ITALIC)
+        self.assertEqual(len(new_nodes), 6)
+        self.assertEqual(new_nodes[0], TextNode("This is ", TextType.TEXT))
+        self.assertEqual(new_nodes[1], TextNode("bold", TextType.BOLD))
+        self.assertEqual(new_nodes[2], TextNode(" text", TextType.TEXT))
+        self.assertEqual(new_nodes[3], TextNode("Another ", TextType.TEXT))
+        self.assertEqual(new_nodes[4], TextNode("italic", TextType.ITALIC))
+        self.assertEqual(new_nodes[5], TextNode(" text", TextType.TEXT))
+
+    
+    
